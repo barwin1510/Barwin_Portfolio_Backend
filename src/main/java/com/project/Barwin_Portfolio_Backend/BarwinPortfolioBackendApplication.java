4204baa92b83aa533412a38ecb6924f8a7c0c5ba
barwin <barwin.raj@althisolutions.com>
2025-04-06 15:24:54 +0530

    Created

---
+++ src/main/java/com/project/Barwin_Portfolio_Backend/BarwinPortfolioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.Barwin_Portfolio_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarwinPortfolioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarwinPortfolioBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/Barwin_Portfolio_Backend/BarwinPortfolioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Barwin_Portfolio_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarwinPortfolioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
